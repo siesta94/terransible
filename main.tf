257fef475dc5a0de5a4360d60a1bbe53998538ff
Strahinja <strahinjatasic@gmail.com>
2019-12-05 17:44:31 +0100

    removing files

--- main.tf
+++ main.tf
@@ -165,12 +165,26 @@ resource "aws_route_table_association" "wp_private2_assoc" {
 resource "aws_security_group" "public_sg"{
   name   = "Allow_public_traffic"
   vpc_id = "${aws_vpc.wp_vpc.id}"
-  
+
+  ingress{
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   ingress{
     from_port = 80
     to_port   = 80
     protocol  = "tcp"
 
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
